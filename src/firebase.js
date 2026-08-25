c89690b267e28e0331ed67cc319972814b618232
h3rus4n15-maker <h3rus4n15@gmail.com>
2026-08-25 10:53:48 +0700

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
-// Ini adalah "brankas" konfigurasi Anda di Vite
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyAHkl_g8tHAKS109IRSCfmKZcs1h7JM2hw",
   authDomain: "pohonkeluargakita-4af0d.firebaseapp.com",
   projectId: "pohonkeluargakita-4af0d",
   storageBucket: "pohonkeluargakita-4af0d.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  messagingSenderId: "247303353629",
+  appId: "1:247303353629:web:879acc0849b421eac75df4"
 };
 
-// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-
-// Ekspor Firestore (database) dan Auth agar bisa dipakai di file lain
 export const db = getFirestore(app);
-export const auth = getAuth(app);
