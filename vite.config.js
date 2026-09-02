5a788a4a9623c0bc70ce60db7a36d86fb6dbcb7d
h3rus4n15-maker <h3rus4n15@gmail.com>
2026-09-02 17:14:28 +0700

    Fix GitHub Pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     VitePWA({
@@ -16,8 +17,8 @@ export default defineConfig({
         background_color: '#0f172a',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: './',
+        start_url: './',
         icons: [
           {
             src: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="%236366f1"><rect width="512" height="512" rx="128" fill="%234f46e5"/><path d="M256 96c-44.2 0-80 35.8-80 80 0 28.3 14.7 53.2 36.9 67.5C148.6 261 96 324.5 96 400h320c0-75.5-52.6-139-116.9-156.5C321.3 229.2 336 204.3 336 176c0-44.2-35.8-80-80-80z" fill="white"/></svg>',
